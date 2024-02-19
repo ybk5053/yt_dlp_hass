420c055c904f8cd41807ae13b53d90c18730027f
ybk5053 <28558154+ybk5053@users.noreply.github.com>
2024-02-19 22:10:26 +0800

    Update config_flow.py

--- custom_components/yt_dlp/config_flow.py
+++ custom_components/yt_dlp/config_flow.py
@@ -2,7 +2,6 @@
 import os
 
 import voluptuous as vol
-import logging
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_FILE_PATH
@@ -10,8 +9,6 @@ from homeassistant.data_entry_flow import FlowResult
 from homeassistant.core import callback
 
 from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow."""
@@ -51,7 +48,6 @@ class OptionsFlow(config_entries.OptionsFlow):
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Initialize options flow."""
         self.config_entry = config_entry
-        self._options = dict(config_entry.options)
 
     async def async_step_init(self, user_input=None) -> FlowResult:
         """Manage the options."""
@@ -65,10 +61,8 @@ class OptionsFlow(config_entries.OptionsFlow):
                 return self.async_show_form(
                     step_id="init", data_schema=vol.Schema({vol.Required(CONF_FILE_PATH): str}), errors=errors
                 )
-            _LOGGER.warn(user_input)
-            self._options.update(user_input)
-            _LOGGER.warn(self._options)
-            return self.async_create_entry(title="YT_DLP", data=self._options)
+            self.hass.config_entries.async_update_entry(self.config_entry, data=user_input)
+            return self.async_create_entry(title="YT_DLP", data=user_input)
 
         return self.async_show_form(
             step_id="init", data_schema=vol.Schema({vol.Required(CONF_FILE_PATH, default=self.config_entry.data[CONF_FILE_PATH]): str}), errors=errors
